bbeb3af67a3094e5e4de782126a575c2f0f4b9c1
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-06-15 17:41:00 +0530

    basic ui ready

---
+++ Client/TutorConnect/src/Landing/Components/Navbar.jsx
@@ -0,0 +1,209 @@
+// import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+// import HomeIcon from "@mui/icons-material/Home";
+import Button from '@mui/material/Button';
+
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import ContactsIcon from "@mui/icons-material/Contacts";
+import logoImg from "../media/logo.png";
+import { Container } from "@mui/system";
+import CustomButton from "./CustomButton";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+} from "@mui/material";
+import { useState } from "react";
+// import { LinkOffTwoTone } from "@mui/icons-material";
+// import Accordion from '@mui/material/Accordion';
+// import AccordionSummary from '@mui/material/AccordionSummary';
+// import AccordionDetails from '@mui/material/AccordionDetails';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+
+
+export const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.type === "Tab" || event.type === "Shift")
+    ) {
+      return;
+    }
+
+    setMobileMenu({ ...mobileMenu, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Solutions", "Features", "Plans", "About Us", "Careers", "Contact"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index === 0 && <TipsAndUpdatesIcon />}
+                  {index === 1 && <FeaturedPlayListIcon />}
+                  {index === 2 && <MiscellaneousServicesIcon />}
+                  {index === 3 && <ListAltIcon />}
+                  {index === 4 && <ContactsIcon />}
+                  {index === 5 && <ConnectWithoutContactIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </Box>
+  );
+
+  const NavLink = styled(Typography)(({ theme }) => ({
+    fontSize: "14px",
+    color: "#4F5361",
+    fontWeight: "bold",
+    cursor: "pointer",
+    "&:hover": {
+      
+    },
+  }));
+  // const CustomAccordian= styled(Accordion)(({ theme }) => ({
+  //   width: "8rem",
+  //   fontSize: "14px",
+  //   color: "#4F5361",
+  //   fontWeight: "bold",
+  //   cursor: "pointer",
+  //   background: 'none',
+  //   boxShadow: 'none',
+  //   "&:hover": {
+
+  //   },
+  // }))
+  const NavbarLinksBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
+
+  const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
+    cursor: "pointer",
+    display: "none",
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.down("md")]: {
+      display: "block",
+    },
+  }));
+
+  const NavbarContainer = styled(Container)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: theme.spacing(5),
+    [theme.breakpoints.down("md")]: {
+      padding: theme.spacing(2),
+    },
+  }));
+
+  const NavbarLogo = styled("img")(({ theme }) => ({
+    cursor: "pointer",
+    width:"17rem",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
+
+  return (
+    <NavbarContainer>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <CustomMenuIcon onClick={toggleDrawer("left", true)} />
+          <Drawer
+            anchor="left"
+            open={mobileMenu["left"]}
+            onClose={toggleDrawer("left", false)}
+          >
+            {list("left")}
+          </Drawer>
+          <NavbarLogo src={logoImg} alt="logo-TutorConnect" />
+        </Box>
+
+        <NavbarLinksBox>
+          <NavLink variant="body2">Solutions</NavLink>
+          {/* <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Solutions</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion> */}
+          <NavLink variant="body2">Features</NavLink>
+          <NavLink variant="body2">Plans</NavLink>
+          <NavLink variant="body2">About Us</NavLink>
+          <NavLink variant="body2">Careers</NavLink>
+          <NavLink variant="body2">Contact</NavLink>
+        </NavbarLinksBox>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+        }}
+      >
+        <CustomButton
+          backgroundColor="#fff"
+          color="#1DA1F2"
+          buttonText="Login"
+          borderColor="#1DA1F2"/>
+        <CustomButton
+          backgroundColor="#1DA1F2"
+          color="#fff"
+          buttonText="Get Started"
+        />
+      </Box>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
